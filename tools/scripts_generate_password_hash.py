59956d70d11cfed791aeccdcf4fc7669a4a86f5e
javad71 <60092712+javad71@users.noreply.github.com>
2026-09-19 22:35:07 +0330

    update

--- tools/scripts_generate_password_hash.py
+++ tools/scripts_generate_password_hash.py
@@ -1,5 +1,5 @@
 import base64, hashlib, os, sys
-password = sys.argv[1] if len(sys.argv) > 1 else input('Password: ')
+password = sys.argv[1] if len(sys.argv) > 1 else input('javad891')
 salt = os.urandom(16)
 digest = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 210_000)
 print(base64.b64encode(salt + digest).decode())
